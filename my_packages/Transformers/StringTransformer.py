4bc46123f0b3ffc70d425ae7e1013de7bddcb72b
Reda Elseidy <reda_ahmed_2003@hotmail.com>
2021-03-13 20:47:44 +0400

    Creating Package and modules

---
+++ my_packages/Transformers/StringTransformer.py
@@ -0,0 +1,6 @@
+def reverse_string(p_string):
+    return [p_string[::-1]]
+
+
+def cap_string(p_string):
+    return  p_string.capitalize()
---
+++ my_packages/Transformers_Operations/Math.py
@@ -0,0 +1,2 @@
+def get_average(p_list):
+    print(sum(p_list)/len(p_list))
